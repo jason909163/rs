b1f8cfc103fe226c0131d473195978ff7b87b332
jason909163 <jason992858@gmail.com>
2024-05-01 11:50:11 +0800

    0501 完成註冊警告全中文

--- route.php
+++ route.php
@@ -4,89 +4,107 @@ $parameter = strtolower($route->getParameter(1));
 // 根據參數決定載入特定頁面並讀取需要的資料
 
 switch ($parameter) {
-case "logout";
-  unset($_SESSION['memberID']);
-  unset($_SESSION['username']);
-  header('Location: login');
-break;
+  case "logout";
+    unset($_SESSION['memberID']);
+    unset($_SESSION['username']);
+    header('Location: login');
+    break;
 
-case "home";
-  if(UserVeridator::isLogin(isset($_SESSION['username'])?$_SESSION['username']:'')){
-    $title = "點數查詢";
-    include('view/header/default.php'); // 載入共用的頁首
-    include('view/body/home.php');     // 載入登入用的頁面
-    include('view/footer/default.php'); // 載入共用的頁尾
-  }else{
-    header('Location: logout');
-  }
-break;
+  case "home";
+    if (UserVeridator::isLogin(isset($_SESSION['username']) ? $_SESSION['username'] : '')) {
+      $title = "點數查詢-歡迎查詢";
+      include('view/header/default.php'); // 載入共用的頁首
+      include('view/body/home.php');     // 載入登入用的頁面
+      include('view/footer/default.php'); // 載入共用的頁尾
+    } else {
+      header('Location: logout');
+    }
+    break;
 
-case "login";
-  if(isset($_POST['submit'])) 
-  {
+  case "login";
+    if (isset($_POST['submit'])) {
       $gump = new GUMP();
 
-      $_POST = $gump->sanitize($_POST); 
+      $_POST = $gump->sanitize($_POST);
 
       $validation_rules_array = array(
-          'username'    => 'required|alpha_numeric|max_len,20|min_len,3',
-          'password'    => 'required|max_len,20|min_len,3'
+        'username'    => 'required|alpha_numeric|max_len,20|min_len,3',
+        'password'    => 'required|max_len,20|min_len,3'
       );
       $gump->validation_rules($validation_rules_array);
 
       $filter_rules_array = array(
-          'username' => 'trim|sanitize_string',
-          'password' => 'trim',
+        'username' => 'trim|sanitize_string',
+        'password' => 'trim',
       );
       $gump->filter_rules($filter_rules_array);
 
       $validated_data = $gump->run($_POST);
 
-      if($validated_data === false) {
-          $error = $gump->get_readable_errors(false);
+      if ($validated_data === false) {
+        $error = $gump->get_readable_errors(false);
       } else {
-          // validation successful
-          foreach($validation_rules_array as $key => $val) {
-              ${$key} = $_POST[$key];
-          }
-          $userVeridator = new UserVeridator();
-          $userVeridator->loginVerification($username, $password);
-          $error = $userVeridator->getErrorArray();
+        // validation successful
+        foreach ($validation_rules_array as $key => $val) {
+          ${$key} = $_POST[$key];
+        }
+        $userVeridator = new UserVeridator();
+        $userVeridator->loginVerification($username, $password);
+        $error = $userVeridator->getErrorArray();
 
-          if(count($error) == 0){
-              $condition = "username = :username";
-              $order_by = "1";
-              $fields = "*";
-              $limit = "LIMIT 1";
-              $data_array = array(":username" => $username);
-              $result = Database::get()->query("members", $condition, $order_by, $fields, $limit, $data_array);
-              $_SESSION['memberID'] = $result[0]['memberID'];
-              $_SESSION['username'] = $username;
-              header('Location: home');
-              exit; // 確保停止後續程式碼執行
-          }
+        if (count($error) == 0) {
+          $condition = "username = :username";
+          $order_by = "1";
+          $fields = "*";
+          $limit = "LIMIT 1";
+          $data_array = array(":username" => $username);
+          $result = Database::get()->query("members", $condition, $order_by, $fields, $limit, $data_array);
+          $_SESSION['memberID'] = $result[0]['memberID'];
+          $_SESSION['username'] = $username;
+          header('Location: home');
+          exit; // 確保停止後續程式碼執行
+        }
       }
-  }
-  $title = "點數查詢";
-  include('view/header/default.php'); // 載入共用的頁首
-  include('view/body/login.php');     // 載入登入用的頁面
-  include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    }
+    $title = "點數查詢-登入";
+    include('view/header/default.php'); // 載入共用的頁首
+    include('view/body/login.php');     // 載入登入用的頁面
+    include('view/footer/default.php'); // 載入共用的頁尾
+    break;
 
   case "register":
     // 註冊處理
-    if(isset($_POST['submit'])) 
-      {
-      // 表單驗證
-      $gump = new GUMP();
+    if (isset($_POST['submit'])) {
+      // 手动检查用户名字段是否为空
+      if (isset($_POST['submit'])) {
+        // 手动检查用户名字段是否为空
+        if (empty($_POST['username'])) {
+          $error[] = '手機號碼為必填項目。';
+        }
+        if (empty($_POST['email'])) {
+          $error[] = 'email為必填項目。';
+        }
+        if (empty($_POST['password'])) {
+          $error[] = '密碼為必填項目。';
+        }
+        if (empty($_POST['passwordConfirm'])) {
+          $error[] = '確認密碼為必填項目。';
+        }
 
-        $_POST = $gump->sanitize($_POST); 
+        // 手动检查密码和确认密码是否匹配
+        if ($_POST['password'] !== $_POST['passwordConfirm']) {
+          $error[] = '確認密碼與密碼不匹配。';
+        }
+
+        // 表单验证
+        $gump = new GUMP();
+        $_POST = $gump->sanitize($_POST);
 
         $validation_rules_array = array(
-          'username'    => 'required|alpha_numeric|max_len,20|min_len,8',
-          'email'       => 'required|valid_email',
-          'password'    => 'required|max_len,20|min_len,8',
-          'passwordConfirm' => 'required'
+          'username'    => 'alpha_numeric|max_len,20|min_len,8',
+          'email'       => 'valid_email',
+          'password'    => 'max_len,20|min_len,8',
+          'passwordConfirm' => 'max_len,20|min_len,8'
         );
         $gump->validation_rules($validation_rules_array);
 
@@ -100,11 +118,12 @@ case "login";
 
         $validated_data = $gump->run($_POST);
 
-        if($validated_data === false) {
-          $error = $gump->get_readable_errors(false);
+        // 如果表单验证失败或者错误数组不为空，则显示错误消息
+        if ($validated_data === false || !empty($error)) {
+          $error = array_merge((array)$error, $gump->get_readable_errors(false));
         } else {
           // validation successful
-          foreach($validation_rules_array as $key => $val) {
+          foreach ($validation_rules_array as $key => $val) {
             ${$key} = $_POST[$key];
           }
           $userVeridator = new UserVeridator();
@@ -112,83 +131,84 @@ case "login";
           $userVeridator->isUsernameDuplicate($username);
           $userVeridator->isEmailDuplicate($email);
           $error = $userVeridator->getErrorArray();
-        } 
+        }
+
         //if no errors have been created carry on
-        if(count($error) == 0)
-        {
+        if (count($error) == 0) {
           try {
             // 新增到資料庫
             $data_array = array(
               'username' => $username,
               'password' => $password,
               'email' => $email,
-
             );
             Database::get()->insert("members", $data_array);
 
-            //redirect to index pag
+            //redirect to index page
             header('Location: register?action=joined');
-            exit; // 重定向後確保停止後續程式碼執行
-            
-          //else catch the exception and show the error.
-          } catch(PDOException $e) {
-              $error[] = $e->getMessage();
+            exit; // 重定向后确保停止后续代码执行
+
+            //else catch the exception and show the error.
+          } catch (PDOException $e) {
+            $error[] = $e->getMessage();
           }
         }
       }
-  
-    $title = "點數查詢";
+    }
+
+    $title = "點數查詢-註冊";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/register.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
+
 
   case "call":
     $title = "聯絡我們";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/call.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   case "shop":
     $title = "電商連結";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/shop.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   case "site":
     $title = "座位介紹";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/site.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   case "drink":
     $title = "餐飲價目表";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/drink.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   case "cash":
     $title = "包台價目表";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/cash.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   case "index":
     $title = "首頁";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/index.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 
   default:
     $title = "404";
     include('view/header/default.php'); // 載入共用的頁首
     include('view/body/404.php');
     include('view/footer/default.php'); // 載入共用的頁尾
-  break;
+    break;
 }
--- validators/UserVeridator.php
+++ validators/UserVeridator.php
@@ -1,119 +1,124 @@
 <?php
+
 /**
  * 耦合使用 Database 物件進行資料庫驗證 username 與 email 是否已存在於資料庫
  */
-class UserVeridator {
+class UserVeridator
+{
 
     private $error;
 
     /**
      * 驗證是否已登入
      */
-    public static function isLogin($username){
-        if($username != ''){
-            return true; 
-        } else{
-            return false; 
+    public static function isLogin($username)
+    {
+        if ($username != '') {
+            return true;
+        } else {
+            return false;
         }
     }
 
     /**
      * 可取出錯誤訊息字串陣列
      */
-    public function getErrorArray(){
+    public function getErrorArray()
+    {
         return $this->error;
     }
 
     /**
      * 驗證二次密碼輸入是否相符
      */
-    public function isPasswordMatch($password, $passwrodConfirm){
-		if ($password != $passwrodConfirm){
+    public function isPasswordMatch($password, $passwrodConfirm)
+    {
+        if ($password != $passwrodConfirm) {
             $this->error[] = '請輸入一樣的密碼.';
             return false;
         }
-		return true;
+        return true;
     }
 
     /**
-    * 驗證帳號密碼是否正確可登入
-    */
-    public function loginVerification($username, $password){
+     * 驗證帳號密碼是否正確可登入
+     */
+    public function loginVerification($username, $password)
+    {
         $result = Database::get()->execute('SELECT * FROM members WHERE username = :username', array(':username' => $username));
-        
+
         // 確保資料庫查詢是否成功
-        if($result) {
-             echo "Database query successful.<br>";
-        
+        if ($result) {
+            echo "Database query successful.<br>";
+
             // 確保是否找到了用戶
-            if(isset($result[0]['username']) and !empty($result[0]['username'])) {
-                 echo "User found in database.<br>";
+            if (isset($result[0]['username']) and !empty($result[0]['username'])) {
+                echo "User found in database.<br>";
+            } else {
+                echo "User not found in database.<br>";
             }
-            else {
-                 echo "User not found in database.<br>";
-            }
-                // 檢查密碼是否匹配
-            if($password === $result[0]['password']){
-                 echo "Password matched.<br>";
-                 // 這裡添加獲取點數的代碼
+            // 檢查密碼是否匹配
+            if ($password === $result[0]['password']) {
+                echo "Password matched.<br>";
+                // 這裡添加獲取點數的代碼
                 $user_point = $result[0]['point'];
-                
+
                 // 將用戶點數保存到 session 中
                 $_SESSION['user_point'] = $user_point;
                 return true;
-            } 
-            else {
-                 echo "Password does not match.<br>";
+            } else {
+                echo "Password does not match.<br>";
             }
-             
         } else {
-             echo "Database query failed.<br>";
+            echo "Database query failed.<br>";
         }
-        
+
         $this->error[] = '使用者名稱或密碼錯誤。';
         return false;
-
-    
     }
 
     /**
      * 驗證帳號是否已存在於資料庫中
      */
-    public function isUsernameDuplicate($username){
+    public function isUsernameDuplicate($username)
+    {
         $result = Database::get()->execute('SELECT username FROM members WHERE username = :username', array(':username' => $username));
-        if(isset($result[0]['username']) and !empty($result[0]['username'])){
-          $this->error[] = '提供的使用者名稱已被使用。';
-          return false;
+        if (isset($result[0]['username']) and !empty($result[0]['username'])) {
+            $this->error[] = '提供的使用者名稱已被使用。';
+            return false;
         }
-		return true;
+
+        return true;
     }
     /**
      * 驗證信箱是否已存在於資料庫中
      */
-    public function isEmailDuplicate($email){
+    public function isEmailDuplicate($email)
+    {
         $result = Database::get()->execute('SELECT email FROM members WHERE email = :email', array(':email' => $email));
-        if(isset($result[0]['email']) AND !empty($result[0]['email'])){
+        if (isset($result[0]['email']) and !empty($result[0]['email'])) {
             $this->error[] = '提供的電子郵件已被使用。';
             return false;
         }
-		return true;
-    }   
+        return true;
+    }
 
-        /**
- * 驗證註冊資訊是否正確
- */
-    public function registerVerification($username, $email){
-    // 檢查使用者名稱是否重複
+    /**
+     * 驗證註冊資訊是否正確
+     */
+    public function registerVerification($username, $email)
+    {
+        // 檢查使用者名稱是否重複
         $usernameDuplicate = $this->isUsernameDuplicate($username);
         if (!$usernameDuplicate) {
             return false;
         }
-    
         // 檢查電子郵件是否重複
         $emailDuplicate = $this->isEmailDuplicate($email);
         if (!$emailDuplicate) {
             return false;
         }
+
         // 如果通過以上檢查，返回 true
         return true;
     }
