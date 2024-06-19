646d27388c27b1fdaa45031aa765f1ce399b60e0
jason909163 <jason992858@gmail.com>
2024-06-19 19:10:52 +0800

    1

--- view/body/cash.php
+++ view/body/cash.php
@@ -1,57 +1,55 @@
 <style>
   body {
     overflow-x: hidden;
-    /* 隱藏水平滾動條 */
     margin: 0;
     padding: 0;
   }
 
   .container1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     text-align: center;
-    position: relative;
     width: 100%;
     height: auto;
   }
 
-  .container1 img {
+  .images {
+    justify-content: center;
+    width: 100%;
+  }
+
+  .images img {
     max-width: 100%;
     height: auto;
   }
 
   .cash {
-
     width: 500px;
-    /* 設置具體的寬度 */
-    height: 600px;
-    /* 設置具體的高度 */
-    display: inline-block;
-    vertical-align: top;
+    height: auto;
+    margin: 0 10px;
   }
-
 
   .note {
     margin-left: 5%;
     width: 600px;
-    /* 設置具體的寬度 */
     height: 500px;
-    /* 設置具體的高度 */
     display: inline-block;
     vertical-align: top;
   }
 
   h1 {
-    margin-left: 28%;
-    /* 調整這個值，使標題靠近圖片 */
     margin-bottom: 10px;
-    vertical-align: top;
+    margin-right: 680px;
   }
 </style>
+</head>
 
-<h1>座位價目表</h1>
-<div class="container1">
-  <img src="photo/cash.jpg" alt="cash" class="cash">
-  <img src="photo/note.jpg" alt="note" class="note">
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
+<body>
+  <div class="container1">
+    <h1>座位價目表</h1>
+    <div class="images">
+      <img src="photo/cash.jpg" alt="cash" class="cash">
+      <img src="photo/note.jpg" alt="note" class="note">
+    </div>
+  </div>
